90c41faee79b3f78a5faed1c59b3fcee00c392f7
Mauricio Flores <mauricio@Mauricios-MacBook-Pro.local>
2024-09-24 15:58:26 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data } = await useAsyncData('test', () => queryContent('test').findOne());
+</script>
+
+<template>
+  <div>
+    <h1>Bug Demo: Nuxt Static Site Generation</h1>
+    <p>When running 'npm run generate' or 'nuxt generate', the process breaks when pre-rendering json content that contains 'href' in a string. An example use case is if we have an encoded html string in said content, and wanted to render it in a template. Granted, it's generally not safe, but in a case where we are migrating or importing CMS content with rich text from another platform, this is very plausible.</p>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
